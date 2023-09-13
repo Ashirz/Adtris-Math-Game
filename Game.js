a4fe777cdb892be894b3d29153a3b50db6cb2071
Ashirz <121093841+Ashirz@users.noreply.github.com>
2023-09-13 15:54:03 +0100

    Update Game.js - 3

--- Game.js
+++ Game.js
@@ -1,15 +1,20 @@
 // Game.js
 
 let blocks = [];
+let tetrominos = [];
 let targetNumber = 0;
 let currentSum = 0;
 let score = 0;
 let level = 1;
 let placedBlock = null;
+let placedTetro = null;
+
+
 let instructionsScreen;
 
-let blockPlacementSound;
-let levelCompletionSound;
+
+//let blockPlacementSound;
+//let levelCompletionSound;
 
 let startButton;
 let levelSelection;
@@ -20,21 +25,9 @@ let exitButton;
 
 
 
-// Load sound files
-/*function preload() {
-    blockPlacementSound = loadSound('..Sound/block_placement.mp3');
-   // levelCompletionSound = loadSound('path_to_your_sound_files/level_completion.mp3');
-}*/
-
-
-/*function preload() {
-   console.log('Preloading sound files...');
-  
-}*/
-
-
 function setup() {
-  createCanvas(400, 550);
+  
+   createCanvas(650, 550);
   background(255, 230, 230);
   newLevel();
   levelSelection = select('#level-selection');
@@ -44,106 +37,161 @@ function setup() {
   startButton = select('#start-btn');
   setupLevelSelection();
   setupInstructionsScreen();
-   /* instructionsScreen = select('#instructions-screen');
-    instructionsScreen.mousePressed(startGame);*/
-    // Hide the canvas initially and show level selection options
    
-
- // backgroundMusic = loadSound("Sound/awake10_megaWall.mp3");
-  // backgroundMusic.play();
-  // Start playing background music if it's loaded
- /* if (backgroundMusic.isLoaded()) {
-    backgroundMusic.loop();
-  }*/
    // Initialize the exit button element
   exitButton = select('#exit-button');
   exitButton.mousePressed(exitLevel);
-  }
+   
+   }
   
   function exitLevel() {
-  levelSelection.show();
-   newLevel();
-    
+   levelSelection.show();
+      newLevel();
+ 
 }
   function setupLevelSelection() {
   level1Button.mousePressed(() => {
     levelSelection.hide();
     instructionsScreen.show();
+    setupInstructionsScreen();
+     console.log(currentSum);
     targetNumber = Math.floor(random(11, 40)); // Set target number for level 1
+  //  targetNumber = Math.floor(Math.random() * (40 - 1) + 1);
     // Other level-specific properties/settings
+      generateBlocks();
+    
   });
 
   level2Button.mousePressed(() => {
     levelSelection.hide();
     instructionsScreen.show();
-    targetNumber = Math.floor(random(41, 70)); // Set target number for level 2
+    setupInstructionsScreen();
+  targetNumber = Math.floor(random(41, 70)); // Set target number for level 2
     // Other level-specific properties/settings
+        generateTetrominos();
+    
   });
 }
 
 function setupInstructionsScreen() {
   startButton.mousePressed(() => {
     instructionsScreen.hide();
-   // startGame();
-  });
+   });
 }
   
-/*function startGame() {
-    instructionsScreen.hide();
-    // Initialize game components and logic here
-}*/
+
 function draw() {
     background(220);
   // Display game components based on the selected level
      if (selectedLevel === 1) {
+      
       // Execute level 1 logic
       setupLevelSelection(); // Call a function that contains level 1 logic
       
+      
     } else if (selectedLevel === 2) {
+     
       // Execute level 2 logic
       setupLevelSelection(); // Call a function that contains level 2 logic
+      
         }
-  
-  displayBlocks();
+        
+   displayBlocks();
+ displayTetros();
   displayGrid();
   displayUI();
-
-  // Check if background music is playing, and if not, play it
- /* if (!backgroundMusic.isPlaying() && backgroundMusic.isLoaded()) {
-    backgroundMusic.loop();
-  }*/
-  
+   
 }
-
-
 
 function mousePressed() {
   
-  // Handle mousePressed logic
+    // Handle mousePressed logic for blocks
   for (let block of blocks) {
     if (block.contains(mouseX, mouseY)) {
-      let blockCopy = new Block(block.x, block.y, block.width, block.color, block.value);
+      let blockCopy = new Block(block.x, block.y, block.size, block.color, block.shapeMatrix, block.value);
       blockCopy.dragging = true;
       blocks.push(blockCopy);
-      
+      break;
+    }
+  }
+
+  // Handle mousePressed logic for tetrominos
+  for (let tetromino of tetrominos) {
+    if (tetromino.contains(mouseX, mouseY)) {
+      let tetrominoCopy = new Tetromino(
+        tetromino.x,
+        tetromino.y,
+        tetromino.size,
+        tetromino.color,
+        tetromino.shapeMatrix,
+        tetromino.value
+      );
+      tetrominoCopy.dragging = true;
+      tetrominos.push(tetrominoCopy);
       break;
     }
   }
 }
-
+  
 function mouseDragged() {
-  // Handle mouseDragged logic
+  // Handle mouseDragged logic for blocks
   for (let block of blocks) {
     if (block.dragging) {
       block.x = mouseX;
       block.y = mouseY;
     }
   }
+  // Handle mouseDragged logic for tetrominos
+  for (let tetromino of tetrominos) {
+    if (tetromino.dragging) {
+      tetromino.x = mouseX;
+      tetromino.y = mouseY;
+    }
+  }
 }
 
 function mouseReleased() {
-  // Handle mouseReleased logic
-   let placedBlock = null;
+  // Handle mouseReleased logic for Tetrominos
+  let placedTetro = null;
+
+  for (let tetromino of tetrominos) {
+    if (tetromino.dragging) {
+      tetromino.dragging = false;
+      placedTetro = tetromino;
+      
+      break;
+    }
+  }
+
+  if (placedTetro !== null) {
+    
+    // Implement logic to check if the Tetromino can be placed within the grid boundaries
+              if (currentSum + placedTetro.value <= targetNumber) {
+      // Calculate the grid row index based on the block's y position
+     
+      let gridY = Math.floor((placedTetro.y - 190) / 30);
+
+      // Check if the block is placed within the boundaries of the grid
+      if (gridY >= 0 && gridY <= 10 && 
+        mouseX >= 115 && mouseX <= 525 &&
+        mouseY >= 190 && mouseY <= 480)
+       {
+      placedTetro.placedOnGrid = true;
+      currentSum += placedTetro.value;
+      checkWinCondition(); // You may want to modify this function to handle Tetrominos
+    } else {
+      // Tetromino cannot be placed within grid boundaries, revert its placement
+      placedTetro.placedOnGrid = false;
+      alert("Place Tetromino on grid!");
+    }
+  }else {
+      alert("Target exceeded, Try again!"); // Alert the player if the sum exceeds the target
+      console.log(placedTetro.value);
+      newLevel(); // Start a new level after the alert
+    }
+  }
+  
+  let placedBlock = null;
 
   for (let block of blocks) {
     if (block.dragging) {
@@ -157,16 +205,15 @@ function mouseReleased() {
     if (currentSum + placedBlock.value <= targetNumber) {
       // Calculate the grid row index based on the block's y position
      
-      let gridY = Math.floor((placedBlock.y - 65) / 30);
+      let gridY = Math.floor((placedBlock.y - 190) / 30);
 
       // Check if the block is placed within the boundaries of the grid
       if (gridY >= 0 && gridY <= 12 && 
-        mouseX >= 15 && mouseX <= 350 &&
-        mouseY >= 65 && mouseY <= 485)
+        mouseX >= 115 && mouseX <= 525 &&
+        mouseY >= 190 && mouseY <= 480)
        {
         // Update sum only if the block is placed within the grid
         placedBlock.placedOnGrid = true;
-       // blockPlacementSound.play(); // Play sound effect
         currentSum += placedBlock.value;
         checkWinCondition();
       } else {
@@ -176,21 +223,21 @@ function mouseReleased() {
       }
     } else {
       alert("Target exceeded, Try again!"); // Alert the player if the sum exceeds the target
+     
       newLevel(); // Start a new level after the alert
     }
   }
 }
+
 
 function checkWinCondition() {
   // Handle checkWinCondition logic
   if (currentSum === targetNumber) {
     score++;
     level++;
-    // levelCompletionSound.play(); // Play sound effect
     alert("Congrats, Target Achieved!"); // Feedback message
     newLevel();
   } else if (currentSum > targetNumber) {
-   // showCustomAlert(); // Display the alert when target sum is exceeded
     newLevel(); // Start a new level after the alert
   }
 }
@@ -200,29 +247,26 @@ function displayUI() {
   textSize(16);
   textAlign(LEFT, CENTER);
   fill(0);
-  text(`Target: ${targetNumber}`, 20, 55);
- text(`Remaining: ${targetNumber-currentSum}`, width - 250, 55);
-  text(`Score: ${score}`, width-100, 55);
-   text(`A D T R I S : ${selectedLevel}`, width-250,510); 
+   text(`Level : ${selectedLevel} `, 550,160);
+ text(`Target: ${targetNumber}`, 550, 200);
+//  text(`Math.floor(Math.random() * (40 - 1) + 1`,20,55);
+ text(`Remains: ${targetNumber-currentSum}`, 550, 240);
+  text(`Score: ${score}`, 550, 280);
+  
+  text(`ADTRIS`, 50,160);
+   
  }
   
-
-function displayBlocks() {
-  for (let block of blocks) {
-    block.display();
-  }
-}
-
-
 function displayGrid() {
   // Display grid logic
    let cellSize = 30;
-  let gridWidth = 360; // Total width of the grid (30*12)
-  let gridHeight = 420; // Total height of the grid (30*14)
+  let gridWidth = 420; // Total width of the grid (30*14)
+  let gridHeight = 300; // Total height of the grid (30*10)
 
   // Calculate the starting positions to center-align the grid
   let startX = (width - gridWidth) / 2;
-  let startY = (height - gridHeight) / 2;
+ // let startY = (height - gridHeight) / 2;
+  let startY = 190;
 
   for (let x = startX; x < startX + gridWidth; x += cellSize) {
     for (let y = startY; y < startY + gridHeight; y += cellSize) {
@@ -235,30 +279,142 @@ function displayGrid() {
 
 function newLevel() {
   // Generate a new level logic
-   blocks = [];
-  currentSum = 0;
- // targetNumber = Math.floor(random(11, 40)); // Generate a new random target number (1-50)
-  generateBlocks(targetNumber);
-  
-}
-
-function generateBlocks(target) {
   blocks = [];
-  let blockSizes = [1, 2, 3, 4];
- 
-  let colors = ['#FF5733', '#33FF5E', '#3391FF', '#E433FF'];
-  
-  let totalBlockWidth = blockSizes.reduce((total, size) => total + size * 30 + 5, -5); // Calculate the total width of all blocks
-  
-  // Calculate the starting x position to center-align the blocks
-  let startX = (width - totalBlockWidth) / 2;
-  let y = 10;
-  
-  for (let size of blockSizes) {
-    let value = size;
-    let blockWidth = size * 30; // Fixed width for simplicity
-    let colorIndex = size - 1;
-    blocks.push(new Block(startX, y, blockWidth, colors[colorIndex], value));
-    startX += blockWidth + 5;
+  tetrominos = [];
+  currentSum = 0;
+ }
+
+/*function displayBlocks() {
+  for (let block of blocks) {
+    if (!block.placedOnGrid) {
+      block.display();
+    }
   }
 }
+
+function displayTetros() {
+  for (let tetromino of tetrominos) {
+    if (!tetromino.placedOnGrid) {
+      tetromino.display();
+    }
+  }
+}*/
+
+function displayBlocks() {
+  for (let block of blocks) {
+    block.display();
+  }
+}
+
+function displayTetros() {
+  for (let tetromino of tetrominos) {
+    tetromino.display();
+  }
+}
+
+
+/*function generateTetrominos() {
+  tetrominos = []; // Clear existing tetrominos
+
+  const tetrominoShapes = [
+    {
+      shape: 'L-shape',
+      color: '#FF5733',
+      blocks: [
+        new Tetromino(0, 0, 'L-shape', '#FF5733'),
+        new Tetromino(0, 1, 'L-shape', '#FF5733'),
+        new Tetromino(0, 2, 'L-shape', '#FF5733'),
+        new Tetromino(1, 2, 'L-shape', '#FF5733'),
+      ],
+    },
+    {
+      shape: 'T-shape',
+      color: '#33FF5E',
+      blocks: [
+        new Tetromino(0, 0, 'T-shape', '#33FF5E'),
+        new Tetromino(-1, 1, 'T-shape', '#33FF5E'),
+        new Tetromino(0, 1, 'T-shape', '#33FF5E'),
+        new Tetromino(1, 1, 'T-shape', '#33FF5E'),
+      ],
+    },
+    {
+      shape: 'square',
+      color: '#3391FF',
+      blocks: [
+        new Tetromino(0, 0, 'square', '#3391FF'),
+        new Tetromino(1, 0, 'square', '#3391FF'),
+        new Tetromino(0, 1, 'square', '#3391FF'),
+        new Tetromino(1, 1, 'square', '#3391FF'),
+      ],
+    },
+    // Add more tetromino shapes as needed
+  ];
+
+  const startX = 100; // Starting x position for tetrominos
+  const startY = 100; // Starting y position for tetrominos
+
+  for (const shapeData of tetrominoShapes) {
+    const tetromino = new Tetromino(shapeData.shape, shapeData.color);
+    tetromino.blocks = shapeData.blocks; // Set the blocks for the tetromino
+    for (const block of shapeData.blocks) {
+      block.x += startX;
+      block.y += startY;
+    }
+    tetrominos.push(tetromino); // Add the tetromino to the array
+  }
+}*/
+
+
+/*function generateTetrominos() {
+  tetrominos = []; // Clear existing tetrominos
+
+  const tetrominoShapes = [
+    {
+      shape: 'L-shape',
+      color: '#FF5733',
+      blocks: [
+      new Tetromino(4, 0, '#FF5733'),
+        new Tetromino(4, 1, '#FF5733'),
+        new Tetromino(4, 2, '#FF5733'),
+        new Tetromino(5, 2, '#FF5733'),
+            ],
+    }, 
+    {
+      shape: 'T-shape',
+      color: '#33FF5E',
+      blocks: [
+         new Tetromino(4, 0, '#33FF5E'),
+        new Tetromino(3, 1, '#33FF5E'),
+        new Tetromino(4, 1, '#33FF5E'),
+        new Tetromino(5, 1, '#33FF5E'),
+      ],
+    },
+    {
+      shape: 'square',
+      color: '#3391FF',
+      blocks: [
+        new Tetromino(4, 0, '#3391FF'),
+        new Tetromino(5, 0, '#3391FF'),
+        new Tetromino(4, 1, '#3391FF'),
+        new Tetromino(5, 1, '#3391FF'),
+      ],
+    },
+    // Add more tetromino shapes as needed
+  ];
+  
+  const startX = 100; // Starting x position for tetrominos
+  const startY = 100; // Starting y position for tetrominos
+
+  for (const shapeData of tetrominoShapes) {
+    const tetromino = new Tetromino(shapeData.shape, shapeData.color);
+    for (const block of shapeData.blocks) {
+      block.x += startX;
+      block.y += startY;
+    }
+    tetromino.blocks = shapeData.blocks; // Set the blocks for the tetromino
+   // tetrominos.push(tetromino); // Add the tetromino to the array
+    tetrominos.push(new Tetromino('L-shape', '#FF5733'));
+    tetrominos.push(new Tetromino('T-shape', '#33FF5E'));
+    tetrominos.push(new Tetromino('square', '#3391FF'));
+  }
+}*/
